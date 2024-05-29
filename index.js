f2b765e7cd405f966fb2b75e315f1bceb755877c
Thanish Ahamed <thanish@hatchyard.io>
2024-05-29 14:43:37 +0530

    add data

--- index.js
+++ index.js
@@ -1,58 +1,47 @@
 const express = require('express')
 const app = express();
+const cors = require('cors');
 const { NlpManager } = require('node-nlp');
+const bodyParser = require('body-parser');
 
-const manager = new NlpManager({ languages: ['en'], forceNER: true });
+app.use(cors());
+
+app.use(bodyParser.json({}));
 
 app.get('/', (req, res) => {
     res.send('welcome');
 })
 
-// app.get('/train', async (req, res) => {
-// // Adds the utterances and intents for the NLP
-// manager.addDocument('en', 'who is the developer of this application', 'about');
-// manager.addDocument('en', 'Who is the developer of this application', 'about');
-// manager.addDocument('en', 'Who is Thanish?', 'about');
-// manager.addDocument('en', 'Open account', 'account.open');
-// // manager.addDocument('en', 'bye bye take care', 'greetings.bye');
-// // manager.addDocument('en', 'okay see you later', 'greetings.bye');
-// // manager.addDocument('en', 'bye for now', 'greetings.bye');
-// // manager.addDocument('en', 'i must go', 'greetings.bye');
-// // manager.addDocument('en', 'hello', 'greetings.hello');
-// // manager.addDocument('en', 'hi', 'greetings.hello');
-// // manager.addDocument('en', 'howdy', 'greetings.hello');
+app.post('/train', async (req, res) => {
+    const token = req.body.token;
+    const trainData = req.body.trainData;
+    const manager = new NlpManager({ languages: ['en'], forceNER: true });
 
-// // Train also the NLG
-// manager.addAnswer('en', 'acount.open', 'fn:open');
-// manager.addAnswer('en', 'about', 'Thanish');
-// manager.addAnswer('en', 'about', 'Thanish and some of the batch mates');
-// // manager.addAnswer('en', 'greetings.bye', 'see you soon!');
-// // manager.addAnswer('en', 'greetings.hello', 'Hey there!');
-// // manager.addAnswer('en', 'greetings.hello', 'Greetings!');
+    trainData.forEach(dt => {
+        dt.questions.forEach(que => manager.addDocument('en', que, dt.intent));
+        dt.answers.forEach(ans => manager.addAnswer('en', dt.intent, ans));
+    })
 
-// // Train and save the model.
-// (async() => {
-//     await manager.train();
-//     manager.save();
-// })();
+    (async() => { 
+        await manager.train();
+        manager.save('./models/'+token+'.nlp');
+    })();
 
-// res.json({message: "Processed!"});
-// })
+    res.json({message: "Processed!"});
+})
 
-app.get('/get/:message', async (req, res) => {
+app.post('/get', async (req, res) => {
     let newManager = new NlpManager();
-    newManager.load('./model.nlp');
-    const response = await newManager.process('en', req.params.message);
+    newManager.load('./models/'+req.body.token+'.nlp');
+
+    const response = await newManager.process('en', req.body.message);
 
     if (response.answers.length === 0) {
-        res.json({message: "Please Train Me!", question: req.params.message})
+        res.json({message: "Please Train Me!", question: req.body.message})
     } else {
         console.log({message: response});
         res.json({message: response.answer})
     }
-    // console.log(response);
-
-    
 })
 
 app.listen(process.env.port || 3000, () => {
